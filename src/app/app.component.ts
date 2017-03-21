adcf29454243becab346f70861854b9218cc1b4d
Venkata Krishna Bandla <venkatakrishnabtech@gmail.com>
2017-03-21 09:58:17 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { GithubService } from './services/github.service';
+
+@Component({
+  selector: 'my-app',
+  template: `<profile></profile>`,
+  providers:[ GithubService ]
+})
+export class AppComponent  {
+
+}
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+
+  private username:string;
+  private clientId = 'd4aa6bb96c29d45718c2';
+  private clientSecret = '9292bfe8d9a8c049d3ed2ed1905d5bfd9ffb3e3f';
+
+  constructor(private _http: Http){
+
+    console.log('Github service ready....');
+    this.username = 'venkatiiitb';
+
+  }
+
+  getUser(){
+
+    return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.clientId+'&client_secret='+this.clientSecret)
+      .map(res => res.json());
+
+  }
+
+}
